61f8b0013cc26cbea9a9de4e4b31e6a8ea696493
Sajeel Khawaja <sajeel_waien@live.com>
2020-08-28 12:05:28 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,15 @@
-import React, { Suspense, useState } from "react";
+import React, {  useState } from "react";
 import ReactDOM from "react-dom";
+import CustomComponent from './custom-component'
+import CustomComponent1 from './custom-component1'
 
-const CustomComponent = React.lazy(
-  () =>
-    new Promise((resolve, reject) =>
-      setTimeout(() => resolve(import("./custom-component")), 100)
-    )
-);
-
-const CustomComponent2 = React.lazy(
-  () =>
-    new Promise((resolve, reject) =>
-      setTimeout(() => resolve(import("./custom-component1")), 5000)
-    )
-);
 
 function App() {
-  const [state, setState] = useState(true);
-  const handle = () => {
-    setState(!state);
-  };
   return (
     <>
-      <button onClick={handle}>toggle</button>
-
-      <Suspense fallback={<div>Loading</div>}>
         <CustomComponent label="Component 1" />
-      </Suspense>
-
-      <Suspense fallback={<div>Loading</div>}>
         <CustomComponent2 label="Component 2" />
-      </Suspense>
+
     </>
   );
 }
