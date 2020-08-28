ded322f52e9249b4d5a993d67db67cbf071266ef
Sajeel Khawaja <sajeel_waien@live.com>
2020-08-28 12:44:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CustomComponent from './custom-component'
 import CustomComponent1 from './custom-component1'
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom'
 
 
 
@@ -12,7 +12,9 @@ function App() {
            
        <Router>
       <Switch>
-        <Route path='/' render={props => <div > Root</div>} />
+        <Link to='/comp' >Comp1</Link>
+        <Link to='/comp1' >Comp2</Link>
+        <Route path='/' exact render={props => <div > Root</div>} />
         <Route path='/comp' render={props => <CustomComponent label="Component 1" {...props} />} />
         <Route path='/comp1' render={props => <CustomComponent1 label="Component 2" {...props} />} /> 
       </Switch>
