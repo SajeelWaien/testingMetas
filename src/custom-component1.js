5a54f3751286ca8c761303ffe84cbbf275644ebf
Sajeel Khawaja <sajeel_waien@live.com>
2020-08-28 12:03:54 -0400

    Update custom-component1.js

--- src/custom-component1.js
+++ src/custom-component1.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
-import MetaTags, { ReactTitle } from "react-meta-tags";
+import MetaTags from "react-meta-tags";
 const CustomComponent1 = ({ label }) => {
   useEffect(() => {
     console.log(
       `${label} created at ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
     );
     return () => console.log(`${label} destroyed`);
-  }, []);
-  // const metaTags = metaTagsInstance.getTags();
+  }, [label]);
   return (
     <div>
       <MetaTags>
@@ -19,7 +18,6 @@ const CustomComponent1 = ({ label }) => {
         <meta property="twitter:description" content="Some change description" />
         <meta property="twitter:title" content="some change more titile " />
       </MetaTags>
-      {/* <ReactTitle title="Page 2"/> */}
 
       {label}
     </div>
