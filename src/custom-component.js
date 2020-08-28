e552027d24ab78b76dc41e636b435804ce63ffc3
Sajeel Khawaja <sajeel_waien@live.com>
2020-08-28 12:38:48 -0400

    Update custom-component.js

--- src/custom-component.js
+++ src/custom-component.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-
+import MetaTags from "react-meta-tags";
 const CustomComponent = ({ label }) => {
   useEffect(() => {
     console.log(
@@ -7,8 +7,21 @@ const CustomComponent = ({ label }) => {
     );
     return () => console.log(`${label} destroyed`);
   }, [label]);
+  return (
+    <div>
+      <MetaTags>
+        <title>Page 1</title>
+        <meta name="description" content="Comp2 description." />
+        <meta property="og:title" content="Comp2 change" />
+        <meta property="og:image" content="Comp2 pathchange/to/image.jpg" />
+        <meta property="og:description" content="Comp2 Somechange description" />
+        <meta property="twitter:description" content="Comp2 Some change description" />
+        <meta property="twitter:title" content="Comp2 some change more titile " />
+      </MetaTags>
 
-  return <div>{label}</div>;
+      {label}
+    </div>
+  );
 };
 
 export default CustomComponent;
